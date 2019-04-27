fe64573d99b441cfb0d25fbd2a42fc087739c5cf
HarrisunW <47761871+HarrisunW@users.noreply.github.com>
2019-04-27 20:43:35 +1000

    Update CentralityMeasures.c

--- CentralityMeasures.c
+++ CentralityMeasures.c
@@ -4,39 +4,138 @@
 #include "PQ.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
-NodeValues outDegreeCentrality(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
+static NodeValues newNode(int nV);
+
+NodeValues outDegreeCentrality(Graph g) {
+    if (g == NULL) {
+        NodeValues throwAway = {0};
+        return throwAway;
+    }
+    NodeValues out = newNode(numVerticies(g));
+    for (int i = 0; i < numVerticies(g); i++) {
+        for (AdjList curr = outIncident(g,i); curr != NULL; curr = curr->next) {
+            out.values[i] = out.values[i] + 1;
+        }
+    }
+    return out;
 }
-NodeValues inDegreeCentrality(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
+
+NodeValues inDegreeCentrality(Graph g) {
+    if (g == NULL) {
+        NodeValues throwAway = {0};
+        return throwAway;
+    }
+    NodeValues in = newNode(numVerticies(g));
+    for (int i = 0; i < numVerticies(g); i++) {
+        for (AdjList curr = inIncident(g,i); curr != NULL; curr = curr->next) {
+            in.values[i] = in.values[i] + 1;
+        }
+    }
+    return in;
 }
+
 NodeValues degreeCentrality(Graph g) {
-	NodeValues throwAway = {0};
-	return throwAway;
+    if (g == NULL) {
+        NodeValues throwAway = {0};
+        return throwAway;
+    }
+    NodeValues dc = newNode(numVerticies(g));
+    for (int i = 0; i < numVerticies(g); i++) {
+        dc.values[i] = outDegreeCentrality(g).values[i]+inDegreeCentrality(g).values[i];
+    }
+    return dc;
 }
 
-NodeValues closenessCentrality(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
+NodeValues closenessCentrality(Graph g) {
+    if (g == NULL) {
+        NodeValues throwAway = {0};
+        return throwAway;
+    }
+    NodeValues closeness = newNode(numVerticies(g));   
+    double reach = 0;
+    double total = 0;
+    for (int src = 0; src < numVerticies(g); src++) {
+        ShortestPaths p = dijkstra(g, src);
+        reach = 0;
+        total = 0;
+        for (int dest = 0; dest < numVerticies(g); dest++) {
+            if (src != dest) {
+                if (p.dist[dest] > 0) {
+                    reach++;
+                    total = total + p.dist[dest];
+                } 
+            } else {
+                reach++;
+            }
+        }        
+        if (total == 0) {
+            closeness.values[src] = 0;
+        } else {
+            closeness.values[src] = ((reach - 1)/(numVerticies(g) - 1))*((reach - 1)/total);
+        }
+    }
+    return closeness;
 }
 
 NodeValues betweennessCentrality(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
+    if (g == NULL) {
+        NodeValues throwAway = {0};
+        return throwAway;
+    }
+    int count1, count2;
+    double value;
+    NodeValues betweenness = newNode(numVerticies(g));
+    for (int i = 0; i < numVerticies(g); i++) {
+        for (int j = 0;j < numVerticies(g); j++) {
+            ShortestPaths p = dijkstra(g, i);
+            for (int k = 0; p.pred[k] != NULL; k++) {
+                for (int l = 0; p.pred[k][l].next != NULL; l++) {
+                    if (p.pred[k][l].v == i) {
+                        count2++;
+                    }
+                    count1++;
+                }
+            }
+        }
+        value = count1/count2;
+        betweenness.values[i] = value;
+    }
+    return betweenness;
 }
 
 NodeValues betweennessCentralityNormalised(Graph g){
-	NodeValues throwAway = {0};
-	return throwAway;
+    if (g == NULL) {
+        NodeValues throwAway = {0};
+        return throwAway;
+    }
+    NodeValues normalised = newNode(numVerticies(g));   
+    NodeValues bc = betweennessCentrality(g);
+    for (int i = 0; i < numVerticies(g); i++) {
+        normalised.values[i] = bc.values[i]/((numVerticies(g) - 1)*(numVerticies(g) - 2));
+    }
+	return normalised;
 }
 
 void showNodeValues(NodeValues values){
-
+    for (int i = 0; i < values.noNodes; i++){
+        printf("%d: %lf\n", i, values.values[i]);
+    }
 }
 
 void freeNodeValues(NodeValues values){
+    free(values.values);
+}
 
+// Set a new node value
+static NodeValues newNode(int nV){
+    NodeValues new;
+    new.noNodes = nV;
+    new.values = malloc(nV*sizeof(double));
+    assert(new.values != NULL);
+    for (int i = 0; i < nV; i++){
+        new.values[i] = 0;
+    }
+    return new;
 }
